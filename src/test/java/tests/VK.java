0ece21e3901fa65a24771cc5f9765cf9c0607eb4
bchertok <boris.chertok@mail.ru>
2018-10-03 14:40:49 +0300

    structure for autotests

---
+++ src/test/java/tests/VK.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import  com.codeborne.selenide.commands.*;
+class VK extends TestBase{
+    @Test void vk() {
+
+    }
+}
